93605c2579658f489a6e44cb5d0d8a26a427e191
saronn4 <sara_turkey19@hotmail.com>
2023-07-16 11:35:22 +0400

    Deployed website

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import reportWebVitals from "./reportWebVitals";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import { HashRouter } from "react-router-dom";
 
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter base="/">
   <App/>
-  </BrowserRouter>,
+  </HashRouter>
 )
-
-reportWebVitals();
-
