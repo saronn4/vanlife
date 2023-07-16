64471a193e1e27a1fbb21215aef42026c1d791dc
saronn4 <sara_turkey19@hotmail.com>
2023-07-16 11:33:47 +0400

    Deployed website

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import reportWebVitals from "./reportWebVitals";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
- <App/>
-  </React.StrictMode>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <App/>
+  </BrowserRouter>,
 )
+
+reportWebVitals();
+
